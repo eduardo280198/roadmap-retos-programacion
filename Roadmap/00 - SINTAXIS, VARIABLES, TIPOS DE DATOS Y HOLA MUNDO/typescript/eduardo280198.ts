355b1a96c5ca5010fcf32c7b81ba35555be26d97
Eduardo Lozano Alvarado <eduardo_280198@hotmail.com>
2024-01-22 22:11:03 -0600

    #00 - Typescript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/typescript/eduardo280198.ts
@@ -0,0 +1,21 @@
+// Documentacion Oficial: https://www.typescriptlang.org
+
+// Comentario de una linea
+
+/*
+* Comentario
+* de
+* Multiples
+* Lineas
+*/
+
+let myVariable : string = 'Typescript'
+const myConstant : string = 'Typescript';
+
+let stringType  : string = 'string';
+let numberType  : number = 0;
+let booleanType : boolean = true;
+let nullType    : null = null;
+let undefinedType: undefined = undefined;
+
+console.log(`Hola ${myVariable}!!!`);
